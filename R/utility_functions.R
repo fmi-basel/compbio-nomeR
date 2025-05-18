1f7fdf7a7d4072291f5759b9f7f13cbbcdb2484a
Charlotte Soneson <charlottesoneson@gmail.com>
2025-05-18 19:37:38 +0200

    Linting/styling - utility_functions

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,38 +1,30 @@
-
-
-
 ## function for converting vector of start priors to cover priors
 .start_prior2cover_prior <- function(start_prior,
-                                     footprint_len){
-  stopifnot(length(start_prior) == length(footprint_len))
-  
-  cover_prior <- start_prior * footprint_len
-  cover_prior/sum(cover_prior)
+                                     footprint_len) {
+    stopifnot(length(start_prior) == length(footprint_len))
+    
+    cover_prior <- start_prior * footprint_len
+    cover_prior / sum(cover_prior)
 }
 
 ## function for converting vector of covert priors to start priors
 .cover_prior2start_prior <- function(cover_prior,
-                                     footprint_len){
-  stopifnot(length(cover_prior) == length(footprint_len))
-  stopifnot(all(footprint_len > 0))
-  
-  
-  start_prior <- cover_prior/footprint_len
-  start_prior/sum(start_prior)
+                                     footprint_len) {
+    stopifnot(length(cover_prior) == length(footprint_len))
+    stopifnot(all(footprint_len > 0))
+    
+    start_prior <- cover_prior / footprint_len
+    start_prior/sum(start_prior)
 }
 
-
-.message_timestamp <- function(msg){
-  message(paste0("[",Sys.time(),"]: ",msg))
+.message_timestamp <- function(msg) {
+    message(c("[", Sys.time(), "]: ", msg))
 }
 
 .warning_timestamp <- function(msg){
-  warning(paste0("[",Sys.time(),"]: ",msg))
+    warning(c("[", Sys.time(), "]: ", msg))
 }
 
-
-.onUnload <- function (libpath) {
-  library.dynam.unload("nomeR", libpath)
+.onUnload <- function(libpath) {
+    library.dynam.unload("nomeR", libpath)
 }
-
-
