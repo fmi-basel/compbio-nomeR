d5207970aba21cba5a9e9bbca819554fd1b5c783
EvgOz <evgeniy.ozonov@gmail.com>
2026-05-18 15:35:36 +0200

    fix bug in getPosteriorDecodingFtpConf

--- nomeR.Rcheck/00_pkg_src/nomeR/tests/testthat/test-get_ctable_from_matrix.R
+++
@@ -1,7 +0,0 @@
-test_that("get_ctable_from_matrix works", {
-	dlist <- readRDS(test_path("testdata/test-predict_footprints_data.rds"))
-	ctbl <- get_ctable_from_matrix(dlist$test_dat_mat)
-
-	expect_equal(ctbl, as.matrix(dlist$exp_ctable))
-
-})
--- nomeR.Rcheck/tests/testthat/test-get_ctable_from_SE.R
+++
@@ -1,7 +0,0 @@
-test_that("get_ctable_from_SE works", {
-	## load data
-	dlist <- readRDS(test_path("testdata/test-predict_footprints_SE_data.rds"))
-	ctbl <- get_ctable_from_SE(dlist$test_se)
-	expect_equal(ctbl,dlist$exp_ctable)
-
-})
